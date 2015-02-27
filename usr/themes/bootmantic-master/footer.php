b9743a126c7170ea55801ed561b708037634dce2
CDog34 <i@cdog.me>
2015-02-27 15:08:46 +0800

    细节调整
    去除body背景，改为原生界面
    调整information位置（上移）
    修改copyright信息

--- usr/themes/bootmantic-master/footer.php
+++ usr/themes/bootmantic-master/footer.php
@@ -8,8 +8,8 @@
             <?php _e('Theme By <a href="http://blog.sloger.info" target="_blank">Sloger</a>. '); ?>
             <?php _e('Adapted By <a href="http://cdog.me" target="_blank">CDog</a>. '); ?>
         </p>
-<p><a href=" http://www.miitbeian.gov.cn/" rel="nofollow">苏ICP备13006442号-2</a></p>
-        <p>囧夏字幕组 版权所有 | <a href="http://code.xkacg.com">夏空社IT开发组搞基策划小分队制作</a></p>
+<p></p>
+        <p>囧夏字幕组 版权所有 | <a href=" http://www.miitbeian.gov.cn/" rel="nofollow">苏ICP备13006442号-2</a> | <a href="http://code.xkacg.com">夏空社IT开发组搞基策划小分队制作</a></p>
     </div>
 </footer>
 
