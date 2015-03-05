e8132c64aea5a1f101e1d4c4e5e644234a8d6a3c
CDog34 <i@cdog.me>
2015-03-05 23:26:25 +0800

    Add a link to footer

--- usr/themes/bootmantic-master/footer.php
+++ usr/themes/bootmantic-master/footer.php
@@ -6,7 +6,7 @@
         <p>
             &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
             <?php _e('Theme By Sloger</a>. '); ?>
-            <?php _e('Adapted By <a href="http://cdog.me" target="_blank">CDog</a>. '); ?>
+            <?php _e('Adapted By <a href="http://cdog.me" target="_blank">CDog</a> & <a href="http://yuetsh.com/" target="_blank">yue/tsh</a> . '); ?>
         </p>
 <p></p>
         <p>囧夏字幕组 版权所有 | <a href=" http://www.miitbeian.gov.cn/" rel="nofollow" target="_blank">苏ICP备13006442号-2</a> | <a href="http://code.xkacg.com" target="_blank">夏空社IT开发组搞基策划小分队制作</a></p>
